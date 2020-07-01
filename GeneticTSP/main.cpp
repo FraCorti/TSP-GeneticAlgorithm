490d34dd9ef927c5ba7531e9bb0ed42ab988eadc
Francesco Corti <cortifrancesco05@gmail.com>
2020-07-01 20:19:27 +0200

    update of main file

--- GeneticTSP/main.cpp
+++ GeneticTSP/main.cpp
@@ -1,6 +1,7 @@
 #include "src/graph/graph.h"
 #include "src/tsp/sequentialTSP.h"
 #include "src/tsp/parallelTSP.h"
+#include "src/tsp/fastflowTSP.h"
 
 #define INITTIME   auto start = std::chrono::high_resolution_clock::now();\
   auto elapsed = std::chrono::high_resolution_clock::now() - start;\
@@ -36,10 +37,13 @@ int main(int argc, char *argv[]) {
   }
 
   Graph<> graph(nodesNumber, seed);
-  TSPSequential<int, double> tspSequential(graph);
-  tspSequential.Run(chromosomesNumber, generationNumber, mutationRate, crossoverRate, workerNumber, seed);
+  //TSPSequential<int, double> tspSequential(graph);
+  //tspSequential.Run(chromosomesNumber, generationNumber, mutationRate, crossoverRate, workerNumber, seed);
 
-  TSPParallel<int, double> tspParallel(graph);
-  tspParallel.Run(chromosomesNumber, generationNumber, mutationRate, crossoverRate, workerNumber, seed);
+  //TSPParallel<int, double> tspParallel(graph);
+  //tspParallel.Run(chromosomesNumber, generationNumber, mutationRate, crossoverRate, workerNumber, seed);
+
+  TSPFastflow<int, double> tspFastflow(graph);
+  tspFastflow.Run(chromosomesNumber, generationNumber, mutationRate, crossoverRate, workerNumber, seed);
   return 0;
 }
